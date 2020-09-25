3ab24f914943956ce5f839ff23cd80e3a8b0df91
Jay Womack <womackjw@hotmail.com>
2020-09-24 20:03:27 -0700

    initial commit. Configure two views for about and home

---
+++ django_blog/django_blog/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url(r'^about/$', views.about),
+    url(r'^$', views.homepage),
+]
