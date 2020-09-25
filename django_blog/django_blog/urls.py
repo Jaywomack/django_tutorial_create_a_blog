fc0df3ddd963e9c0999b3d4d026011400bcdf935
Jay Womack <womackjw@hotmail.com>
2020-09-24 20:32:51 -0700

    Created a app within the main app called articles and routed it in the main urls file so that django knows to use the articles app

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -1,10 +1,12 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^articles/', include('articles.urls')),
     url(r'^about/$', views.about),
     url(r'^$', views.homepage),
+
 ]
